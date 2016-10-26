61184c3f450ab86788cd0a13c650a64fdd72f9bb
cremones <francesco.cremonesi@epfl.ch>
2016-10-26 19:50:15 +0200

    whitening and PRI-DER data

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -2,45 +2,53 @@
 """some helper functions."""
 import numpy as np
 
-def standardize(x, mean_x=None, std_x=None):
+
+def load_data(sub_sample=True, add_outlier=False):
+    """Load data and convert it to the metric system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+    return height, weight, gender
+
+
+def sample_data(y, x, seed, size_samples):
+    """sample from dataset."""
+    np.random.seed(seed)
+    num_observations = y.shape[0]
+    random_permuted_indices = np.random.permutation(num_observations)
+    y = y[random_permuted_indices]
+    x = x[random_permuted_indices]
+    return y[:size_samples], x[:size_samples]
+
+
+def standardize(x):
     """Standardize the original data set."""
-    if mean_x is None:
-        mean_x = np.mean(x, axis=0)
+    mean_x = np.mean(x, axis=0)
     x = x - mean_x
-    if std_x is None:
-        std_x = np.std(x, axis=0)
-    x[:, std_x>0] = x[:, std_x>0] / std_x[std_x>0]
-    
-    tx = np.hstack((np.ones((x.shape[0],1)), x))
-    return tx, mean_x, std_x
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x, mean_x, std_x
 
 
-def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-    num_batches_max = int(np.ceil(data_size/batch_size))
-    if num_batches is None:
-        num_batches = num_batches_max
-    else:
-        num_batches = min(num_batches, num_batches_max)
+def de_standardize(x, mean_x, std_x):
+    """Reverse the procedure of standardization."""
+    x = x * std_x
+    x = x + mean_x
+    return x
 
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
